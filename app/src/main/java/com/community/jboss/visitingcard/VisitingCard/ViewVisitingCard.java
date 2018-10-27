a4a19c2537a54d8df0f74747a8e70fc20656ac0e
Alejandro Bertinelli <alexberti02@gmail.com>
2018-10-27 17:17:29 +0200

    GCI 2018: Added Visiting Card Layout and links to social media
    **This PR is part of the Google Code-in 2018 contest.**
    
    Added a simple Visiting Card (encapsulated in a CardView) which is included in the `ViewVisitingCard` activity. Apart from name, email, profile picture and phone number, there are functional links to GitHub user page, LinkedIn profile and Twitter profile (with fictional usernames and name).
    
    Fixes #8
    
    Tested on Android Emulator using API 19 (KitKat 4.4) and API 28 (Pie 9.0).

--- app/src/main/java/com/community/jboss/visitingcard/VisitingCard/ViewVisitingCard.java
+++ app/src/main/java/com/community/jboss/visitingcard/VisitingCard/ViewVisitingCard.java
@@ -1,13 +1,14 @@
 package com.community.jboss.visitingcard.VisitingCard;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
-import com.community.jboss.visitingcard.Maps.MapsActivity;
 import com.community.jboss.visitingcard.R;
 
 public class ViewVisitingCard extends AppCompatActivity {
@@ -28,5 +29,26 @@ public class ViewVisitingCard extends AppCompatActivity {
         });
 
         //TODO: Add ImageView and a number of TextViews to display Selected Visiting Card Information.
+    }
+
+    public void onClick(View view) {
+        String text = ((TextView) view).getText().toString().trim();
+        String url;
+        switch (view.getId()){
+            case R.id.tvGithub:
+                url = getString(R.string.viewcard_github) + text;
+                break;
+            case R.id.tvLinkedin:
+                url = getString(R.string.viewcard_linkedin) + text;
+                break;
+            case R.id.tvTwitter:
+                url = getString(R.string.viewcard_twitter) + text;
+                break;
+            default:
+                return;
+        }
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        startActivity(i);
     }
 }
